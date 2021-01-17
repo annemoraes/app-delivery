1b3206c46efc73dfe810543dd34bd4315de0a6f6
annemoraes <ilarymoraes@hotmail.com>
2021-01-17 14:34:08 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <SafeAreaView>
+
+      </SafeAreaView>
+    </>
+  );
+};
+
+
+export default App;
